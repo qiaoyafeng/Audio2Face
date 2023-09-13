3a0d4435a8210f584c625a6a19a470ceb1e51631
qiaoyafeng <qiaoyafeng@gmail.com>
2023-09-13 15:46:07 +0800

    refactor: remove user model

--- models.py
+++ models.py
@@ -39,11 +39,3 @@ class ChatOutput:
         self.tts = {}
         self.face = {}
         self.motion = {}
-
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(32), unique=True, index=True)
-    hashed_password = Column(String(32))
-    is_active = Column(Boolean, default=True)
